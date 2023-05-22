4c407c09b31287b540beded2394a4a649acba27f
Vsalihu1 <121818806+Vsalihu1@users.noreply.github.com>
2023-05-22 23:23:41 +0200

    Create ReservationDaoImpl3.java

---
+++ ReservationDaoImpl3.java
@@ -0,0 +1,32 @@
+public class ReservationDaoImpl implements ReservationDao{
+List<Reservation> reservations;
+public ReservationDaoImpl3 (){
+reservations=new ArrayList<Reservation>();
+Reservation reservation1=new Reservation(1,"E zene");
+Reservation reservation2=new Reservation(2,"E lire");
+Reservation reservation3=new Reservation(3,"E zene");
+Reservation reservation4=new Reservation(4,"E lire");
+reservations.add(reservation1);
+reservations.add(reservation2);
+reservations.add(reservation3);
+reservations.add(reservation4);
+}
+@Override
+public void deleteReservation(Reservation reservation){
+reservation.remove(reservation.getroomNumber());
+System.out.println("Room number: "+reservation.getroomNumber()+",updated in the database");
+}
+@Override
+public List<Reservation>getAllReservations(){
+return reservations;
+}
+@Override
+public Reservation getReservation(int roomNumber){
+return Rerevation.get(roomNumber);
+}
+@Override
+public void updateReservation(Reservation reservation){
+reservations.get(reservation.getroomNumber()).setstatus(bill.getstatus());
+System.out.println("Room number: "+reservation.getroomNumber()+",updated in the database");
+}
+}
