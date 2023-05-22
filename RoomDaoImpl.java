16d50af6dfa9ce7f7f8490cbbe399e9302f2714b
Vsalihu1 <121818806+Vsalihu1@users.noreply.github.com>
2023-05-22 22:33:20 +0200

    Create RoomDaoImpl.java

---
+++ RoomDaoImpl.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+public class RoomDaoImpl implements RoomDao{
+List<Room> rooms;
+public RoomDaoImpl (){
+rooms=new ArrayList<Room>();
+Room room1=new Room(1,"Single");
+Room room2=new Room(2,"Double");
+Room room3=new Room(3,"Single");
+Room room4=new Room(4,"Double");
+rooms.add(room1);
+rooms.add(room2);
+rooms.add(room3);
+rooms.add(rooms4);
+}
+@Override
+public void deleteRoom(Room room){
+rooms.remove(room.getNumber());
+System.out.println("Room: Number"+room.getNumber()+",updated in the database");
+}
+@Override
+public List<Room>getAllRooms(){
+return rooms;
+}
+@Override
+public Room getRoom(int Number){
+return students.get(Number);
+}
+@Override
+public void updateRoom(Room room){
+rooms.get(room.getNumber()).setType(student.getType());
+System.out.println("Room: Number"+room.getNumber()+",updated in the database");
+}
+}
